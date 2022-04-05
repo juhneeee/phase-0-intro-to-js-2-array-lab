33ad753176493478fafc30f6f80f6a60a08fb230
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 13:21:17 -0400

    commit tree 8b0ceeb96ad0fe46d5f6549a92c6c5b66e31eb86

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(n){
+    cats.push(n);
+}
+function destructivelyPrependCat(n){
+    cats.unshift(n);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(n){
+    return [...cats,n]
+}
+function prependCat(n){
+    return [n, ...cats]
+}
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
